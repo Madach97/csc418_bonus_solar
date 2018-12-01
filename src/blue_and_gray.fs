cd81c8e149101434e924ed6e2f4ea1ed0bb76a27
ravich43 <madu.ravichandran@mail.utoronto.ca>
2018-12-01 15:41:55 -0500

    coloured more closely to planet colours

--- src/blue_and_gray.fs
+++ src/blue_and_gray.fs
@@ -13,16 +13,16 @@ void main()
   /////////////////////////////////////////////////////////////////////////////
   // Replace with your code:
   if(is_earth){
-       color = vec3(0,0,1);
+       color = vec3(25.0/255.0,22.0/255.0,224.0/255.0);
   }
   else if(is_venus){
-  	  color = vec3(0.5, 0, 0);
+  	  color = vec3(173.0/255.0, 138.0/255.0, 12.0/255.0);
   }
   else if(is_mercury){
-  	   color = vec3(1, 0, 0);
+  	   color = vec3(229.0/255.0, 41.0/255.0, 27.0/255.0);
   }
   else if(is_sun){
-  	  color = vec3(0, 1, 0);
+  	  color = vec3(224.0/255.0, 180.0/255.0, 22.0/255.0);
   }
   else{
   	  color = vec3(0, 0, 0);
